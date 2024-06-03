fb51f6caba4f3a9caaee699858be1632a2387436
natalia-2023-20 <nataliamadeira11@hotmail.com>
2024-06-03 16:37:13 -0300

    atualização frontend

--- PFinal_v2/Models/Dia.cs
+++ PFinal_v2/Models/Dia.cs
@@ -8,11 +8,15 @@ namespace PFinal_v2.Models
         public int UsuarioId { get; set; }
 
         [Display(Name = "Código de Custo")]
+        [Required(ErrorMessage = "O campo Código de custo é obrigatório")]
         public int WbsId { get; set; }
 
         [Display(Name = "Data")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "O campo Data é obrigatório")]
         public DateTime DiaData { get; set; }
+
+        [Required(ErrorMessage = "O campo Horas é obrigatório")]
         public double Horas { get; set; }
 
         public Usuario? Usuario { get; set; }
