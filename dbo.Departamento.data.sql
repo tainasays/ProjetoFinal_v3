3546408740e6581013ace5b6b3f86a0750510328
Thayna Santos <thaynasant.ana@hotmail.com>
2024-06-10 13:59:12 -0300

    Exclui Views de Delete não utilizadas; envia dbo dos dados mockados

---
+++ dbo.Departamento.data.sql
@@ -0,0 +1,24 @@
+﻿SET IDENTITY_INSERT [dbo].[Departamento] ON
+ 
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (1, N'Recursos Humanos', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (2, N'Full-Stack Developer', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (3, N'Back-End Developer', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (4, N'Front-End Developer', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (5, N'Infraestrutura', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (6, N'Analista de Dados', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (7, N'Testador de Software', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (8, N'Suporte Técnico', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (9, N'Administrativo', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (10, N'Marketing', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (11, N'Financeiro', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (12, N'Comercial', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (13, N'Gerência', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (14, N'Vendas', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (15, N'Planejamento', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (16, N'Pesquisa e Desenvolvimento', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (17, N'Qualidade', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (18, N'Logística', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (19, N'Treinamento e Desenvolvimento', 0)
+INSERT INTO [dbo].[Departamento] ([DepartamentoId], [Nome], [QuantidadeFuncionarios]) VALUES (20, N'Juridico', 0)
+ 
+SET IDENTITY_INSERT [dbo].[Departamento] OFF
---
+++ dbo.Wbs.data.sql
@@ -0,0 +1,24 @@
+﻿SET IDENTITY_INSERT [dbo].[Wbs] ON
+ 
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (1, N'WBS0912010', N'Férias', 0)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (2, N'WBS0912009', N'Day-off', 0)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (3, N'WBS0912008', N'Sem tarefa', 0)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (4, N'WBS0912007', N'Implementação', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (5, N'WBS0912009', N'Desenvolvimento', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (6, N'WBS0912011', N'Planejamento de Projeto', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (7, N'WBS0912012', N'Análise de Requisitos', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (8, N'WBS0912013', N'Design de Sistema', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (9, N'WBS0912014', N'Testes de Unidade', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (10, N'WBS0912015', N'Documentação', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (11, N'WBS0912016', N'Testes de Integração', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (12, N'WBS0912017', N'Suporte a Cliente', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (13, N'WBS0912018', N'Manutenção de Sistema', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (14, N'WBS0912019', N'Desenvolvimento de Funcionalidades', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (15, N'WBS0912020', N'Code Review', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (16, N'WBS0912021', N'Deploy de Aplicação', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (17, N'WBS0912022', N'Monitoramento de Sistema', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (18, N'WBS0912023', N'Backup e Recuperação de Dados', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (19, N'WBS0912024', N'Planejamento de Sprints', 1)
+INSERT INTO [dbo].[Wbs] ([WbsId], [Codigo], [Descricao], [IsChargeable]) VALUES (20, N'WBS0912025', N'Treinamento de Equipe', 1)
+ 
+SET IDENTITY_INSERT [dbo].[Wbs] OFF
